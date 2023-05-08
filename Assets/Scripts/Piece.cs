88663705e122f8fb69eb4d23d09e3fd3b322e829
jhaldors9512 <jacobhaldors@gmail.com>
2023-05-08 10:02:17 -0700

    Day 1 work

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    public Board board { }
+    public Board board { get; private set; }
+    public TetrominoData data { get; private set; }
+    public Vector3Int postion { get; private set; }
+    public void Initalize(Board board, Vector3Int position, TetrominoData data)
+    {
+        this.board = board;
+        this.position;
+        this.data = data;
+    }
 }
