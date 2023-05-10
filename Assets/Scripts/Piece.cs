a8e5de6ec3038700429b32028b43b10415b7e8b7
jhaldors9512 <jacobhaldors@gmail.com>
2023-05-10 09:25:37 -0700

    Day 2 pt 1

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -6,11 +6,21 @@ public class Piece : MonoBehaviour
 {
     public Board board { get; private set; }
     public TetrominoData data { get; private set; }
-    public Vector3Int postion { get; private set; }
+    public Vector3Int[] cells { get; private set; }
+    public Vector3Int position { get; private set; }
+
     public void Initalize(Board board, Vector3Int position, TetrominoData data)
     {
         this.board = board;
-        this.position;
+        this.position = position;
         this.data = data;
+
+        if(this.cells == null){
+            this.cells = new Vector3Int[data.cells.Length];
+        }
+
+        for (int i = 0; i < data.cells.Length; i++) { 
+            this.cells[i] = (Vector3Int)data.cells[i];
+        }
     }
 }
