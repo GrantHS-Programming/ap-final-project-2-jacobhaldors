f9502082244f2042453633508981368acf894839
jhaldors9512 <jacobhaldors@gmail.com>
2023-06-02 09:51:45 -0700

    Day 7777

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -10,19 +10,30 @@ public class Piece : MonoBehaviour
     public Vector3Int position { get; private set; }
     public int rotationIndex { get; private set; }
 
+    public float stepDelay = 1f;
+    public float moveDelay = 0.1f;
+    public float lockDelay = 0.5f;
+    private float stepTime;
+    private float moveTime;
+    private float lockTime;
+
+
     public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
+        this.data = data;
         this.board = board;
         this.position = position;
-        this.data = data;
-        this.rotationIndex = 0;
-
-        if (cells == null){
+        rotationIndex = 0;
+        stepTime = Time.time + stepDelay;
+        moveTime = Time.time + moveDelay;
+        lockTime = 0f;
+        if (cells == null)
+        {
             cells = new Vector3Int[data.cells.Length];
         }
-
-        for (int i = 0; i < data.cells.Length; i++) { 
-            this.cells[i] = (Vector3Int)data.cells[i];
+        for (int i = 0; i < cells.Length; i++)
+        {
+            cells[i] = (Vector3Int)data.cells[i];
         }
     }
     private void Update()
@@ -54,25 +65,59 @@ public class Piece : MonoBehaviour
 
         board.Set(this);
     }
+    private void HandleMoveInputs()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Move(Vector2Int.down))
+            {
+                stepTime = Time.time + stepDelay;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+    }
+    private void Step()
+    {
+        stepTime = Time.time + stepDelay;
+        Move(Vector2Int.down);
+        if (lockTime >= lockDelay)
+        {
+            Lock();
+        }
+    }
     private void HardDrop()
     {
         while (Move(Vector2Int.down))
         {
             continue;
         }
+        Lock();
+    }
+    private void Lock()
+    {
+        board.Set(this);
+        board.ClearLines();
+        board.SpawnPiece();
     }
     private bool Move(Vector2Int translation)
     {
         Vector3Int newPosition = position;
         newPosition.x += translation.x;
         newPosition.y += translation.y;
-
         bool valid = board.IsValidPosition(this, newPosition);
-
-        if (valid){
-            this.position = newPosition;
+        if (valid)
+        {
+            position = newPosition;
+            moveTime = Time.time + moveDelay;
+            lockTime = 0f; 
         }
-
         return valid;
     }
 
@@ -80,7 +125,7 @@ public class Piece : MonoBehaviour
     {
         int originalRotation = rotationIndex;
 
-        rotationIndex = Wrap(rotationIndex + direction, 0, 4);
+        rotationIndex = Wrap(rotationIndex + direction, 0, 2);
         ApplyRotationMatrix(direction);
 
         if (!TestWallKicks(rotationIndex, direction))
